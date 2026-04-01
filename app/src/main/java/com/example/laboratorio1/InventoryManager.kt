d9c970bce3cf14f5d9daf3bb03d1d47509d5f56d
Maria Paulina Astudillo <mpastudillos@puce.edu.ec>
2026-04-01 18:35:11 -0500

    Laboratorio 3 - Inventario Seguro (Paso 1-4 completado)

--- app/src/main/java/com/example/laboratorio1/InventoryManager.kt
+++ app/src/main/java/com/example/laboratorio1/InventoryManager.kt
@@ -24,11 +24,10 @@ fun main(){
     stockQuantity -= itemsSold //Actualización de variable mutable
 
     // 5. Generación de Reporte con Plantilla de Cadena
-    val report = """ --- REPORTE DE INVENTARIO --- 
+        val report = """ --- REPORTE DE INVENTARIO --- 
     ID: $productId | Nombre: $productName 
     Precio Final: $${String.format("%.2f", totalPriceWithTax)} 
     Stock Actual: $stockQuantity 
     Estado: ${if (stockQuantity > 10) "Suficiente" else "Crítico"} ----------------------------- 
     """.trimIndent()
-        println(report)
 }
